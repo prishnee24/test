8d01634226f696a537a1115183e6c1e7bc4907bb
Prishnee Seegoolam <Prseegoolam@umail.utm.ac.mu>
2024-05-02 20:20:21 +0400

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { MenuModule } from 'primeng/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
